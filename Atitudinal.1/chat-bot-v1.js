8cff29ae55c2f1537e20245ed97a5d920f89888a
isacaramigo <124785311+isacaramigo@users.noreply.github.com>
2024-03-05 22:52:56 -0300

    Add files via upload

---
+++ Atitudinal.1/chat-bot-v1.js
@@ -0,0 +1,9 @@
+import { palavrasPraia, palavrasComidas, animalPraia } from "./can-i-answer";
+console.log(animalPraia)
+
+palavrasPraia.matched = 4;
+
+console.log(mostProbablyQuestion())
+
+let questão = "Qual é o clima da praia agora"
+let pergunta = pergunta.split(' ')
